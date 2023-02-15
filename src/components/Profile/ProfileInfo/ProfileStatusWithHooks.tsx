529717c683c6a23b154fefd117248fed9004c822
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-02-15 21:24:41 +0400

    3

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 
 type ProfileStatusPropsType = {
     status: string
@@ -7,6 +7,9 @@ type ProfileStatusPropsType = {
 export const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
     const activateEditMode = () => {
         setEditMode(true)
     }
